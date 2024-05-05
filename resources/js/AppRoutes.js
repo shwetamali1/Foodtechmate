88ae241feec4003d5372c1de6c9dd1a25df63c5b
shweta mali <malishweta7434@gmail.com>
2024-05-05 19:55:40 +0530

    issue fixed for gmail

--- resources/js/AppRoutes.js
+++ resources/js/AppRoutes.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // Import the provider
 import Example from './components/Example';
 import Home from './components/pages/Home';
 import Login from './components/pages/Login';
 import Register from './components/pages/Register';
 
 function AppRoutes() {
-
     return (
         <Router>
-            <Routes>
-                <Route path="/example" element={<Example />} />
-                <Route path="/dashboard" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-              
-            </Routes>
+            <GoogleOAuthProvider clientId="9013125876-rrt8ja2cjv032ru7u4e15c2fba2i9ph2.apps.googleusercontent.com"> {/* Wrap your routes with the provider */}
+                <Routes>
+                    <Route path="/example" element={<Example />} />
+                    <Route path="/dashboard" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </GoogleOAuthProvider>
         </Router>
     );
 }
